5e987dffcee9d18915c23c42feba9187a85a4ffc
Chaichalit Srisawat <boywert@gmail.com>
2019-12-28 01:39:25 +0700

    quick commit

--- main.c
+++ main.c
@@ -347,7 +347,7 @@ void displacement_fields(void)
 
 
 	    P[n].Vel[axes] = vel;
-	    //printf("p:%d d:%d >>> %lg\n",n,axes,vel*sqrt(InitTime));
+	    printf("p:%d d:%d >>> %lg\n",n,axes,vel*sqrt(InitTime));
 	    
 	    //P[n].Disp[axes] = dis;
 	    
